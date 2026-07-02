764137afb48824690202e44417f3eea348b60dbc
Franz Amoroso <franz.amoroso@outlook.de>
2026-07-02 19:21:00 +0200

    configuration file for vercel

--- BE/main.py
+++ BE/main.py
@@ -1,6 +1,7 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from core.route import router 
+from services.filter import shuffle_filter, validate_answer
 app = FastAPI()
 
 app.add_middleware(
@@ -16,3 +17,11 @@ app.include_router(router, prefix="")
 @app.get("/")
 async def root():
     return {"status":"Server attivo."}
+
+@app.get("/shuffle_filter")
+def api_shuffle_filter(department: str, course: str, sub: str):
+    return shuffle_filter(department, course, sub)
+
+@app.post("/validate_answer")
+def api_validate_answer(idQuestion: str, idChoice: str):
+    return validate_answer(idQuestion, idChoice)
--- fe/lib/services/api_service.dart
+++ fe/lib/services/api_service.dart
@@ -3,44 +3,59 @@ import 'package:http/http.dart' as http;
 import '../models/quiz_model.dart';
 
 class ApiService {
-
   final String baseUrl = 'https://dmi-student-lab.vercel.app';
 
   Future<List<QuizModel>> shuffle_filter(String department, String course, String sub) async {
-    final url = Uri.parse('$baseUrl/send');
+    final url = Uri.parse('$baseUrl/shuffle_filter').replace(
+      queryParameters: {
+        'department': department,
+        'course': course,
+        'sub': sub,
+      },
+    );
 
-      final response = await http.post(
+    try {
+      final response = await http.get(
         url,
-        headers : {
-          'Content-Type':'application/json',
-          // 'X-Custom-Secret':'ChiaveAcesso',
+        headers: {
+          'Content-Type': 'application/json',
         },
-        body: jsonEncode ({
-          'department':department,
-          'course':course,
-          'sub':sub
-        }),
       );
 
       if (response.statusCode == 200) {
-        List<dynamic> body = jsonDecode(response.body)['question'];
+        List<dynamic> body = jsonDecode(response.body);
         return body.map((dynamic item) => QuizModel.fromJson(item)).toList();
       } else {
-        throw Exception('Impossibile caricare le domande');
+        throw Exception('Impossibile caricare le domande: Errore del server');
       }
+    } catch (e) {
+      throw Exception('Impossibile caricare le domande: $e');
+    }
   }
-    Future<bool> validate_quest(String idQuestion, String idChoice) async {
-    final response = await http.post(
-    Uri.parse('$baseUrl/validate'),
-    headers :{
-      'content-Type':'application/json'
-    },
-    body: jsonEncode({'idQuestion':idQuestion,'idChoice':idChoice}),
+
+  Future<bool> validate_quest(String idQuestion, String idChoice) async {
+    final url = Uri.parse('$baseUrl/validate_answer').replace(
+      queryParameters: {
+        'idQuestion': idQuestion,
+        'idChoice': idChoice,
+      },
     );
-    if(response.statusCode == 200) {
-          return jsonDecode(response.body)['correct'];
-    } else {
-      throw Exception('Errore validate: ${response.body}');
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body) == true;
+      } else {
+        throw Exception('Errore validate: ${response.body}');
+      }
+    } catch (e) {
+      throw Exception('Errore connessione validazione: $e');
     }
   }
 }
